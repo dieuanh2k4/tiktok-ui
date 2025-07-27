1b0bc97c39b8e6b3f987c880fac698443ae9250f
Nguyễn Diệu Anh <121100269+dieuanh2k4@users.noreply.github.com>
2025-07-27 23:03:22 +0700

    áp dụng PropTypes

--- src/components/Propper/Menu/index.js
+++ src/components/Propper/Menu/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
+import PropTypes from 'prop-types';
 
 import { Wrapper as PropperWrapper } from '~/components/Propper';
 import styles from './Menu.module.scss';
@@ -49,7 +50,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFb 
           <PropperWrapper className={cx('menu-popper')}>
             {history.length > 1 && (
               <Header
-                title="Language"
+                title={current.title}
                 onBack={() => {
                   setHistory((prev) => prev.slice(0, prev.length - 1));
                 }}
@@ -65,5 +66,12 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFb 
     </Tippy>
   );
 }
+
+Menu.prototype = {
+  children: PropTypes.node.isRequired,
+  items: PropTypes.array,
+  hideOnClick: PropTypes.bool,
+  onChange: PropTypes.func,
+};
 
 export default Menu;
