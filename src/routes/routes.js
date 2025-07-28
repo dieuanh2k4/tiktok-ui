af292a8e3bb295ca50bce10d660c985a74932410
Nguyễn Diệu Anh <121100269+dieuanh2k4@users.noreply.github.com>
2025-07-28 15:28:29 +0700

    UI sidebar p1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,11 +6,13 @@ import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 import { HeaderOnly } from '~/layouts';
+import Live from '~/pages/Live';
 
 // public routes: ko cần đăng nhập vẫn xem được
 const publicRoutes = [
   { path: Config.routes.home, component: Home },
   { path: Config.routes.following, component: Following },
+  { path: Config.routes.live, component: Live },
   { path: Config.routes.profile, component: Profile },
   { path: Config.routes.upload, component: Upload, layout: HeaderOnly },
   { path: Config.routes.search, component: Search, layout: null },
